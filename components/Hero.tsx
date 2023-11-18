7fcd48a8decc13baa8317c5991a8af24cafa134e
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-18 15:25:19 +0100

    enroll button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,17 +8,15 @@ const Hero = () => {
         <h1 className="font-bold">
           My Dear Intelligent, Hardworking, Young Nigerian,
         </h1>
-        <h2 className="pt-4">One of these things has brought you here;</h2>
-        <div className="border-b border-gray-600 h-0 w-65 pt-10 pr-10">
-          <ul>
+        <h2 className="pt-4 pb-6">One of these things has brought you here;</h2>
+        <div className="border-b border-gray-600 h-0 w-65 pt-10 pr-10"></div>
+        <div>
+          <ul className="list-disc pl-4 space-y-6">
             {data.map((item) => (
-              <li key={item.id}>
-                <strong>{item.title}</strong>
-              </li>
+              <li key={item.id}>{item.title}</li>
             ))}
           </ul>
         </div>
-
         <div className="border-b border-gray-600 h-0 w-65 pt-10 pr-10"></div>
       </div>
     </div>
